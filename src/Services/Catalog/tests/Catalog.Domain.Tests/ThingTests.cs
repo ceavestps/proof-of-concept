e66eb91848c744f6ebd533f891a4dc6e7c2a2163
Connor Eaves <ceaves@tpsalert.com>
2023-08-29 20:31:11 -0500

    Add Catalog Domain Thing model and tests

---
+++ src/Services/Catalog/tests/Catalog.Domain.Tests/ThingTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Catalog.Domain.Tests;
+
+[TestFixture]
+public class ThingTests
+{
+    [Test]
+    public void Constructor_ValidGuidAndValidName_ShouldSetProperties ()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var name = "Valid Name";
+
+        // Act
+        var thing = new Thing(id, name);
+
+        // Assert
+        thing.Id.Should().Be(id);
+        thing.Name.Should().Be(name);
+    }
+
+    [Test]
+    public void Constructor_EmptyGuidAndValidName_ShouldThrowException ()
+    {
+        // Arrange
+        var id = Guid.Empty;
+        var name = "Valid Name";
+
+        // Act
+        var action = () => { var _ = new Thing(id, name); };
+
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    [TestCase("ab")]
+    public void Constructor_ValidGuidAndInvalidName_ShouldThrowException (string name)
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        // Act
+        var action = () => { var _ = new Thing(id, name); };
+
+        // Assert
+        action.Should().Throw<ArgumentException>();
+    }
+}
